fa0c013b08ad9f8b2c81759166c3fcdae75de85b
CarlDT <carldt.cd@gmail.com>
2023-07-10 15:53:45 +0200

    Remove changeNotifer

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'view_models/app_view_model.dart';
 import 'views/navigation_bottom.dart';
 
 void main() {
   runApp(
-    ChangeNotifierProvider(
-      create: (context) => AppViewModel(),
-      child: const MyApp(),
-    ),
+    const MyApp(),
   );
 }
 
